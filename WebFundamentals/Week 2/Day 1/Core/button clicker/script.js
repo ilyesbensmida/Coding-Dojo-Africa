666d6936d42d30b552781b4f46d1e64e00ee02d1
ilyesbensmida <ilyes.bensmiida@gmail.com>
2023-11-07 19:43:49 +0100

    index

---
+++ WebFundamentals/Week 2/Day 1/Core/button clicker/script.js
@@ -0,0 +1,11 @@
+function logoutFunc(Element){
+    Element.innerText ="Logout" ;
+}
+
+function likeFunc() {
+    alert("Ninja was liked")
+}
+
+function removeFunc(Element){
+    Element.remove();
+}
