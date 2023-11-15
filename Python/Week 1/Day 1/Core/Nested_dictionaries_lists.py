7459b31623c38e4dea82ef3cb6f422cacd9acfb8
ilyesbensmida <ilyes.bensmiida@gmail.com>
2023-11-15 17:06:43 +0100

    Nested_dictionaries_lists

---
+++ Python/Week 1/Day 1/Core/Nested_dictionaries_lists.py
@@ -0,0 +1,64 @@
+# chalenge 1 
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+students[0]['last_name'] ='Bryant'
+sports_directory['soccer'] [0]='Andres'
+z[0]['y']=30
+print(x)
+print(students)
+print(sports_directory)
+print(z)
+
+
+# chalenge 2
+def iterateDictionary(dict):
+    for i in dict :
+           for key in i.keys():
+            print(f"{key} - {i[key]},") 
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+# challenge 3
+#  
+def iterateDictionary2(key_name, some_list) :
+    list=[]
+    for i in some_list :
+        list.append(i[key_name])
+    for j in list : print(j)
+    return list    
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+
+# challenge 4 
+def printInfo(some_dict) :
+    for key in some_dict.keys():
+        print( len(some_dict[key]),key)
+        for value in some_dict[key]:
+            print(value)
+
+    return None
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+
+       
