0d7ea5cce23346ece2419dcfb05d6d53f745ddc8
ilyesbensmida <ilyes.bensmiida@gmail.com>
2023-11-20 15:47:03 +0100

    Underttanding_routing

---
+++ Python/Week 1/Day 4/Practice/playground/server.py
@@ -0,0 +1,22 @@
+from flask import Flask , render_template
+
+app =Flask(__name__)
+@app.route('/')
+def hi():
+    return f"welcome flask"
+
+@app.route('/play')
+def play():
+    return render_template("index.html",number=3,color="aqua")
+
+@app.route('/play/<int:number>')
+def play1(number):
+    return render_template("index.html", number=number ,color="aqua")
+
+@app.route('/play/<int:number>/<string:color>')
+def play2(number,color):
+    return render_template("index.html",number=number,color=color)
+
+
+if __name__ =="__main__":
+    app.run(debug=True,port=5000)
--- Python/Week 1/Day 4/Practice/understanding_routing/server.py
+++ Python/Week 1/Day 4/Practice/understanding_routing/server.py
@@ -1,22 +1,21 @@
-from flask import Flask , render_template
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/dojo')
+def hello_dojo():
+    return 'Hello dojo!' 
+@app.route('/say/<string:name>')
+def say_name(name) :
+    return f"hi {name}"
+@app.route('/repeat/<int:number>/<string:name>')
+def say_name_number_times(name,number):
+    return  f"hi {name}\n"*number
+@app.errorhandler(404) 
+def invalid_route(e): 
+    return "Sorry! No response. Try again."       
 
-app =Flask(__name__)
-@app.route('/')
-def hi():
-    return f"welcome flask"
-
-@app.route('/play')
-def play():
-    return render_template("index.html",number=3,color="aqua")
-
-@app.route('/play/<int:number>')
-def play1(number):
-    return render_template("index.html", number=number ,color="aqua")
-
-@app.route('/play/<int:number>/<string:color>')
-def play2(number,color):
-    return render_template("index.html",number=number,color=color)
-
-
-if __name__ =="__main__":
-    app.run(debug=True,port=5000)
+            
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
