ef90101913be18a6d53115c3cca1a447cd5e5930
ilyesbensmida <ilyes.bensmiida@gmail.com>
2023-12-23 20:55:17 +0100

    first_react_with_jsx

---
+++ MERN-Stack/MERNStack/week_1/Day_3/core/First_React_With_JSX/first_react_with_jsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello Dojo!</h1>
+    <h3>Things I need to do :</h3> 
+    <p>*Learn React</p>
+    <p>*Climb Mt.Eve</p>
+    <p>*Run a marathon</p>
+    <p>*Feed the dogs</p>
+    </div>
+  );
+}
+
+export default App;
+
